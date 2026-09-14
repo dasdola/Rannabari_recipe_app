5ec45c659613c0e40f434d5a1899901f043fb98e
Sathi <dasdola007@gmail.com>
2026-09-14 13:04:56 +0600

    Add banner widget

---
+++ lib/Views/widget/banner.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:rannabari_recipe_app/utilities/constant.dart';
+
+class BannerTo extends StatelessWidget {
+  const BannerTo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 190,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: KbannerColor,
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 32,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Cook the best\nrecipes at home",
+                  style: TextStyle(
+                    height: 1.1,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 33),
+                    backgroundColor: Colors.white,
+                    elevation: 0,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Explore",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            bottom: 0,
+            right: -20,
+            child: Image.network("https://thumbs.dreamstime.com/b/female-chef-presenting-food-14985514.jpg"),
+          ),
+        ],
+      ),
+    );
+  }
+}
