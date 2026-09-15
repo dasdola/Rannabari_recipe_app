bbeb4d429a41de1e51eb384c05c161884751ceba
Sathi <dasdola007@gmail.com>
2026-09-15 23:32:25 +0600

    Register AuthProvider and wire up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rannabari_recipe_app/Provider/auth_provider.dart';
+import 'package:rannabari_recipe_app/Provider/favorite_provider.dart';
+import 'package:rannabari_recipe_app/Provider/quantity.dart';
 import 'package:rannabari_recipe_app/Views/app_main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const AppMain());
+  runApp(const MyApp());
 }
-
-class AppMain extends StatelessWidget {
-  const AppMain({super.key});
-
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+ 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AppMainScreen(),
-    );
+    return MultiProvider(
+      providers: [
+        // for favorite provider
+        ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+        // for quantity provider
+        ChangeNotifierProvider(create: (_) => QuantityProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AppMain(),
+      ), 
+    ); 
   }
 }
